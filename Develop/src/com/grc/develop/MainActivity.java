2d98116397d180a2cb46ef5126bfbc8ac852baea
Gustavo Coelho <gutorc@hotmail.com>
2013-11-06 20:18:40 +1100

    The first screem.

--- Develop/src/com/grc/develop/MainActivity.java
+++ Develop/src/com/grc/develop/MainActivity.java
@@ -1,8 +1,11 @@
 package com.grc.develop;
 
+import android.net.wifi.WifiConfiguration;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button bt = (Button) findViewById(R.id.button1);
+		bt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				WifiConfiguration wifi = new WifiConfiguration();
+				
+				
+			}
+		});
+		
 	}
 
 	@Override
